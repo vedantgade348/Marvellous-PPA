29b8a27256b72c6e63ccb8fa98675b93a9404d50
Vedant Gade <vedantgade348@gmail.com>
2026-07-19 01:19:38 +0530

    Add files via upload

---
+++ Access.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+
+class Demo
+{
+    public:
+        int i;
+    private:
+        int j;
+    protected:
+        int k;
+
+    public:
+        Demo()
+        {
+            i = 0;
+            j = 0;
+            k = 0;
+        }
+
+        void Display()
+        {
+            cout<<"Value of i : "<<i<<"\n"; // Allowed
+            cout<<"Value of j : "<<j<<"\n"; // Allowed
+            cout<<"Value of k : "<<k<<"\n"; // Allowed
+        }
+};
+
+int main()
+{
+    Demo dobj;
+
+    dobj.Display();
+            
+    cout<<"Value of i : "<<dobj.i<<"\n";    // Allowed
+    cout<<"Value of j : "<<dobj.j<<"\n";    // Not allowed
+    cout<<"Value of k : "<<dobj.k<<"\n";    // Not allowed
+
+    return 0;
+}
